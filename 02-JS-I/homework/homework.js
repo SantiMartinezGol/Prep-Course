94e0ca630ab453db1bceb802e38db188e0a96eed
SantiagoMartinezGol <santimartinez0580@gmail.com>
2022-05-27 03:27:25 -0300

    homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,7 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-   if (str.length(str1)===str.length(str2)){
+   if (str1.length===str2.length){
       return true;
    }
   return false;
@@ -228,7 +228,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
- if (str.length(letra)>1){
+ if (letra.length>1){
    return "Dato incorrecto";
  } else if ((letra==='a') || (letra==='e') || (letra==='i') || (letra==='o') || (letra==='u')) {
    return "Es vocal"
