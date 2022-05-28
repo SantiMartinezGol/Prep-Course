35758531a143b16008167fa13fc05b0456db6174
SantiagoMartinezGol <santimartinez0580@gmail.com>
2022-05-28 05:04:44 -0300

    asd

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -80,10 +80,10 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero === 10 || numero === 5){
+  if ((numero === 10) || (numero === 5)){
     return 'true';
   }
- return 'false'
+ return 'false';
 }
 
 function estaEnRango(numero) {
